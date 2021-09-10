13e5afaa8d80df5151557b834c2ccb1c52f8f2ae
BrouWilliam <brouwilliam@hotmail.com>
2021-09-10 19:11:35 -0300

    aula 38

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,61 @@
 import * as React from "react"
+import { useStaticQuery, graphql } from 'gatsby'
 import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
 import Seo from "../components/seo"
 import PostItem from "../components/PostItem"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <PostItem 
-          slug="/about/"
-          category="Misc"
-          date="30 de Julho de 2019"
-          timeToRead="5"
-          title="Diga não ao Medium: tenha sua própria plataforma"
-          description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
-    />
-  </Layout>
-)
+const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(
+    graphql`
+    query PostList {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              background
+              category
+              description
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              title
+            }
+            timeToRead
+          }
+        }
+      }
+    }
+    
+    `
+  )
+
+  const postList = allMarkdownRemark.edges
+
+  return (
+    <Layout>
+        <Seo title="Home" />
+        {postList.map(({
+          node: {
+            frontmatter: { background, category, description, date, title },
+            timeToRead
+          }
+        }) => (
+        <PostItem 
+              slug="/about/"
+              background={background}
+              category={category}
+              date={date}
+              timeToRead={timeToRead}
+              title={title}
+              description={description}
+        />
+        ))}
+        
+      </Layout>
+  )
+  
+
+}
+
 
 export default IndexPage

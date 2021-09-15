f36f593c0089579959df9a2ea18d8e8e0ecd108c
BrouWilliam <brouwilliam@hotmail.com>
2021-09-14 23:01:12 -0300

    error webpack

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import SEO from '../components/seo'
+
 
 const AboutPage = () => (
     <Layout>
-        <SEO title="About"/>
+        
         <h1>About Page</h1>
     </Layout>
 )
